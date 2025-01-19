3d616fdef1a83961e79708ebc81a47423aaf8e36
Scott Baker <scott@perturb.org>
2025-01-19 15:53:53 -0800

    Add new hashing algorithms

--- rdtsc_rand.h
+++ rdtsc_rand.h
@@ -6,6 +6,51 @@
 // https://github.com/scottchiefbaker/rdtsc_rand
 ////////////////////////////////////////////////////////////////////////////////
 
+// Borrowed and (slightly modified) from
+// https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/hash.h
+// Does not pass SmallCrush
+uint64_t hash64(uint64_t val) {
+	return (val * 0x61c8864680b583ebull);
+}
+
+// Multiply-Shift Hash (Passes SmallCrush)
+uint64_t hash_msh(uint64_t x) {
+    uint64_t prime = 0x9e3779b97f4a7c15; // A large prime constant
+    x ^= (x >> 30);
+    x *= prime;
+    x ^= (x >> 27);
+    x *= prime;
+    x ^= (x >> 31);
+    return x;
+}
+
+// MurmurHash3 Finalizer (Passes SmallCrush)
+uint64_t hash_mur3(uint64_t x) {
+    x ^= x >> 33;
+    x *= 0xff51afd7ed558ccd;
+    x ^= x >> 33;
+    x *= 0xc4ceb9fe1a85ec53;
+    x ^= x >> 33;
+    return x;
+}
+
+// FNV-1a / Fowler–Noll–Vo hash (Does not pass SmallCrush)
+uint64_t hash_fnv(uint64_t x) {
+    uint64_t prime = 1099511628211;           // 64-bit FNV prime
+    uint64_t hash  = 14695981039346656037ULL; // 64-bit FNV offset basis
+
+    for (int i = 0; i < 8; i++) {
+        hash ^= (x & 0xff);
+        hash *= prime;
+        x >>= 8;
+    }
+    return hash;
+}
+
+//////////////////////////////////////
+// End hashing function definitions //
+//////////////////////////////////////
+
 #if defined(_WIN32) || defined(_WIN64)
 #include <intrin.h>
 #pragma intrinsic(__rdtsc)
@@ -28,12 +73,6 @@ uint64_t get_rdtsc() {
 #else
 	#error "Unsupported platform"
 #endif
-}
-
-// Borrowed and (slightly modified) from
-// https://elixir.bootlin.com/linux/v6.11.5/source/include/linux/hash.h
-uint64_t hash64(uint64_t val) {
-	return (val * 0x61c8864680b583ebull);
 }
 
 // Get an unsigned 64bit random integer
